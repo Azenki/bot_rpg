2ca03d764822820d40fd9dc17bad6ed2611fb53f
azenki <azncharles@zone-dxd.fr>
2022-07-13 01:02:53 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(os.getenv('TOKEN'))
